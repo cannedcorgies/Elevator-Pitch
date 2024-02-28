483db5e42447f99c8de45776388508558fc4be4a
ashley-perez <ashleyperez428@gmail.com>
2024-02-27 16:47:27 -0800

    elevator almost done

--- Assets/Scripts/Behaviours/ElevatorAction.cs
+++ Assets/Scripts/Behaviours/ElevatorAction.cs
@@ -10,18 +10,15 @@ public class ElevatorAction : MonoBehaviour
 
     public GameObject target;
 
-    public float slidePower = 10f;
-    public float maxDistance = 5f;
-    public Vector3 maxVector;
+    public float sens = 50f;
+
+    float minZ = -45;
+    float maxZ = 45;
 
     void Start()
     {
-        // Debug.Log("CHECKKKKKKKK BELOW");
-        // Debug.Log( target.name);
-
         activated = false;
         pap = GetComponent<PushAndPull>();
-        
     }
   
     // Update is called once per frame
@@ -31,25 +28,64 @@ public class ElevatorAction : MonoBehaviour
 
         if (activated) {
 
-            var dir = transform.up * move * Time.deltaTime * slidePower;
-            target.transform.position += dir;
+             // if mouse is being pulled down then rotate the lever accordingly
+             if (move < 0) {
+                // rotate based on mouse pulling
+                target.transform.Rotate(0, 0, sens * Time.deltaTime);
 
-            // ==== LIMIT DISPLACEMENT
-            target.transform.position = new Vector3 (Mathf.Clamp(target.transform.position.x, maxVector.x - maxDistance, maxVector.x + maxDistance),
-                                                        Mathf.Clamp(target.transform.position.y, maxVector.y - maxDistance, maxVector.y + maxDistance),
-                                                        Mathf.Clamp(target.transform.position.z, maxVector.z - maxDistance, maxVector.z + maxDistance));
+                // clamp the rotation to a certain range so it doesnt pass a certain range
+                Vector3 currentRotation = target.transform.localEulerAngles;
+                currentRotation.z = ClampAngle(currentRotation.z, minZ, maxZ);
+                target.transform.localEulerAngles = currentRotation;
+             }
+             // if mouse is being pushed move the lever up
+             else if (move > 0) {
+                // rotate based on mouse pulling
+                // in this case invert the movement so it goes up instead of down
+                target.transform.Rotate(0, 0, -sens * Time.deltaTime);
 
+                // clamp the rotation to a certain range so it doesnt pass a certain range
+                Vector3 currentRotation = target.transform.localEulerAngles;
+                currentRotation.z = ClampAngle(currentRotation.z, minZ, maxZ);
+                target.transform.localEulerAngles = currentRotation;
+             }
+
+        }
+
+        // reset the lever so that its back at the top
+        if (NormalizeAngle(target.transform.localEulerAngles.z) >= 40f) {
+            Debug.Log("are we in here???");
+            target.transform.Rotate(0, 0, -70f * Time.deltaTime);
+
+            Vector3 currentRotation = target.transform.localEulerAngles;
+            currentRotation.z = ClampAngle(currentRotation.z, minZ, maxZ);
+            target.transform.localEulerAngles = currentRotation;
+            // Debug.Log(target.name);
         }
         
     }
+
+    // return an angle that is clamped between the range that is passed in
+    float ClampAngle(float angle, float min, float max) {
+        angle = NormalizeAngle(angle);
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    // normalizes the angle so that it is in the range of -180 to 180
+    // instead of 0 to 360
+    float NormalizeAngle(float angle)
+    {
+        while (angle > 180f) angle -= 360f;
+        while (angle < -180f) angle += 360f;
+        return angle;
+    }
+    
 
     void OnDisable()
     {
         
         if (activated) {
-            
             activated = false;
-
         }
 
     }
@@ -59,14 +95,6 @@ public class ElevatorAction : MonoBehaviour
 
         pap.DisableControl(false, false, false);
         activated = true;
-
-        var slidey = target.gameObject.GetComponent<ElevatorComponent>();
-        Debug.Log("this is slidey", slidey);
-        // basically, max distance defined as a square around origin
-        maxVector = slidey.startPos;
-        maxDistance = slidey.maxDistance;
-
-        
 
     }
 
