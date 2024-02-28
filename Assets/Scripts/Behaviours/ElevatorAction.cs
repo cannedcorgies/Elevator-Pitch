bfb273d67e17d3f09283a39fff40bbf204c3fda8
ashley-perez <ashleypere428@gmail.com>
2024-02-27 21:35:04 -0800

    done for now

--- Assets/Scripts/Behaviours/ElevatorAction.cs
+++ Assets/Scripts/Behaviours/ElevatorAction.cs
@@ -27,43 +27,25 @@ public class ElevatorAction : MonoBehaviour
         var move = Input.GetAxis("Mouse Y");
 
         if (activated) {
-
              // if mouse is being pulled down then rotate the lever accordingly
              if (move < 0) {
                 // rotate based on mouse pulling
                 target.transform.Rotate(0, 0, sens * Time.deltaTime);
-
-                // clamp the rotation to a certain range so it doesnt pass a certain range
-                Vector3 currentRotation = target.transform.localEulerAngles;
-                currentRotation.z = ClampAngle(currentRotation.z, minZ, maxZ);
-                target.transform.localEulerAngles = currentRotation;
              }
              // if mouse is being pushed move the lever up
              else if (move > 0) {
                 // rotate based on mouse pulling
                 // in this case invert the movement so it goes up instead of down
                 target.transform.Rotate(0, 0, -sens * Time.deltaTime);
-
-                // clamp the rotation to a certain range so it doesnt pass a certain range
-                Vector3 currentRotation = target.transform.localEulerAngles;
-                currentRotation.z = ClampAngle(currentRotation.z, minZ, maxZ);
-                target.transform.localEulerAngles = currentRotation;
              }
 
-        }
-
-        // reset the lever so that its back at the top
-        if (NormalizeAngle(target.transform.localEulerAngles.z) >= 40f) {
-            Debug.Log("are we in here???");
-            target.transform.Rotate(0, 0, -70f * Time.deltaTime);
-
+            // clamp the rotation to a certain range so it doesnt pass a certain range
             Vector3 currentRotation = target.transform.localEulerAngles;
             currentRotation.z = ClampAngle(currentRotation.z, minZ, maxZ);
             target.transform.localEulerAngles = currentRotation;
-            // Debug.Log(target.name);
         }
-        
     }
+
 
     // return an angle that is clamped between the range that is passed in
     float ClampAngle(float angle, float min, float max) {
@@ -83,20 +65,18 @@ public class ElevatorAction : MonoBehaviour
 
     void OnDisable()
     {
-        
         if (activated) {
             activated = false;
         }
-
     }
 
     void OnEnable()
     {
-
         pap.DisableControl(false, false, false);
         activated = true;
-
     }
 
  
 }
+
+
